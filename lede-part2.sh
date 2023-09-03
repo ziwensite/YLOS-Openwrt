58f869eda9929a67db9a8ad06166f05a1f627e02
ziwensite <yuanliangsong@outlook.com>
2023-09-03 11:29:57 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -9,8 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-
